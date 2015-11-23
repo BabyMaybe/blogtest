6c44270ae3a90367f89153a3335060ad2b429f6e
BabyMaybe <Eric_Varela@mac.com>
2015-11-22 22:54:38 -0500

    initial admin setup

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment
+
+###
+#Admin actions
+###
+def activate(modeladmin, request, queryset):
+    queryset.update(active=True)
+    activate.short_description = "Activate selected entries"
+
+def deactivate(modeladmin, request, queryset):
+    queryset.update(active=False)
+    deactivate.short_description = "Deactivate selected entries"
+
+###
+#Admin classes
+###
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'date_published',  'like_count', 'comment_count', 'active' ]
+    prepopulated_fields = {'slug': ('title',)}
+    actions = [activate, deactivate]
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'display_author', 'content', 'timestamp', 'post', 'active']
+    actions = [activate, deactivate]
+
+# class WYSIWYGAdmin(admin.ModelAdmin):
+#     form = WYSYWIGForm
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+# admin.site.register(WYSIWYG, WYSIWYGAdmin)
+
+
