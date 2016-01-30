0aef4edcb0a33c765f4676fda8d95ec7739852d2
BabyMaybe <Eric_Varela@mac.com>
2016-01-30 02:20:18 -0500

    Removed secret key from public facing repo

--- blog/settings.py
+++ blog/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a$*!#7!v)ct1(z4=yx6^)em(rl_xjvdsk8pcx!n3o@609dwe+_'
+# Disabling secret key for production, moved to local settings
+#SECRET_KEY = 'a$*!#7!v)ct1(z4=yx6^)em(rl_xjvdsk8pcx!n3o@609dwe+_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
